6ff239387aed6453c4d281ff90755dc3765424b9
ZumDeWald <zumdewald@gmail.com>
2021-10-22 12:37:33 -0500

    Player looks left and added more world and NRE enemy

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import Player from './assets/player.png';
 import PlayerAtlas from './assets/player.json';
 import Nav from './assets/nav.png';
 import NavAtlas from './assets/nav.json';
+import Nre from './assets/nre.png';
+import NreAtlas from './assets/nre.json';
 
 class MyGame extends Phaser.Scene
 {
@@ -19,6 +21,7 @@ class MyGame extends Phaser.Scene
         this.load.image('tileSet', TileSet);
         this.load.atlas('player', Player, PlayerAtlas);
         this.load.atlas('nav', Nav, NavAtlas);
+        this.load.atlas('nre', Nre, NreAtlas);
     }
       
     create ()
@@ -34,11 +37,15 @@ class MyGame extends Phaser.Scene
         this.physics.add.collider(this.player, level);
         this.cameras.main.setBounds(0, 0, world.widthInPixels, world.heightInPixels);
         this.cameras.main.startFollow(this.player, true);
-        this.cameras.main.setZoom(1);
+        this.cameras.main.setZoom(2);
 
-        this.nav = this.physics.add.sprite(200, 0, 'nav');
+        this.nav = this.physics.add.sprite(500, 300, 'nav');
         this.nav.setImmovable(true);
         this.physics.add.collider(this.nav, level);
+
+        this.nre = this.physics.add.sprite(250, 200, 'nre');
+        this.nre.setImmovable(true);
+        this.physics.add.collider(this.nre, level);
 
         this.anims.create({key: 'idle', frames: [{key: 'player', frame: 'player_0'}], frameRate: 10});
         this.anims.create({key: 'jump', frames: [{key: 'player', frame: 'player_5'}], frameRate: 10});
@@ -46,8 +53,12 @@ class MyGame extends Phaser.Scene
 
         this.anims.create({key: 'navHit', frames: [{key: 'nav', frame: 'nav_6'}], frameRate: 6});
         this.anims.create({key: 'navIdle', frames: this.anims.generateFrameNames('nav', {prefix: 'nav_', start: 0, end: 5 }), frameRate: 5, repeat: -1});
+
+        this.anims.create({key: 'nreHit', frames: [{key: 'nre', frame: 'nre_6'}], frameRate: 6});
+        this.anims.create({key: 'nreIdle', frames: this.anims.generateFrameNames('nre', {prefix: 'nre_', start: 0, end: 5 }), frameRate: 5, repeat: -1});
         
         this.physics.add.collider(this.player, this.nav);
+        this.physics.add.collider(this.player, this.nre);
 
         this.cursors = this.input.keyboard.createCursorKeys();
     }
@@ -55,11 +66,17 @@ class MyGame extends Phaser.Scene
     update() {
         if(this.cursors.left.isDown) {
             this.player.setVelocityX(-200);
-            if(this.player.body.onFloor()) this.player.play('walk', true);
+            if(this.player.body.onFloor()) {
+                this.player.setFlipX(true);
+                this.player.play('walk', true);
+            }
 
         } else if(this.cursors.right.isDown) {
             this.player.setVelocityX(200);
-            if(this.player.body.onFloor()) this.player.play('walk', true);
+            if(this.player.body.onFloor()) {
+                this.player.setFlipX(false);
+                this.player.play('walk', true);
+            }
 
         } else {
             this.player.setVelocityX(0);
@@ -73,6 +90,9 @@ class MyGame extends Phaser.Scene
         
         if(this.player.body.hitTest(this.nav.body.x, this.nav.body.y)) this.nav.play('navHit', true);
         else this.nav.play('navIdle', true);
+
+        if(this.player.body.hitTest(this.nre.body.x, this.nre.body.y)) this.nre.play('nreHit', true);
+        else this.nre.play('nreIdle', true);
     }
 }
 
